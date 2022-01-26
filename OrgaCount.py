168ebd3a7c77fd86af05f90a668cf80fb7b6d53c
Christina Bukas <ga27loj@mytum.de>
2022-01-26 16:37:57 +0100

    converted to object oriented

---
+++ OrgaCount.py
@@ -0,0 +1,136 @@
+import os
+from aicsimageio import AICSImage
+from skimage.measure import block_reduce
+from scipy import ndimage as ndi
+from skimage.feature import canny
+from skimage.measure import regionprops,label
+from skimage.morphology import opening,remove_small_objects,closing,dilation,erosion
+import numpy as np
+import math
+
+def setup_bboxes(segmentation): 
+    bboxes = []
+    diameter1 = []
+    diameter2 = []
+    i=0
+    for region in regionprops(segmentation):
+        minr, minc, maxr, maxc = region.bbox
+        bbox_rect = np.array([[minr, minc], [maxr, minc], [maxr, maxc], [minr, maxc]])
+        bboxes.append(bbox_rect)
+        diameter1.append(maxr-minr)
+        diameter2.append(maxc-minc)
+    bbox_properties = {'diameter1': diameter1, 'diameter2': diameter2}
+    text_parameters = {
+        'text': 'D1: {diameter1}\nD2: {diameter2}',
+        'anchor': 'upper_left',
+        'translation': [-5, 0],
+        'size': 8,
+        'color': 'green',
+    }
+    bboxes = np.array(bboxes)
+    return bbox_properties, text_parameters, bboxes
+
+def circle_area(r):
+    return math.pi * r**2 
+
+def apply_normalization(img):
+    img_min = np.min(img) # 31.3125 png 0
+    img_max = np.max(img) # 2899.25 png 178
+    img_norm = (255 * (img - img_min) / (img_max - img_min)).astype(np.uint8)
+    return img_norm
+
+class OrgaCount():
+    def __init__(self, root_path, img_path, downsampling_size):
+        img_czi = AICSImage(os.path.join(root_path, img_path))
+        self.downsampling_size = downsampling_size
+        self.img_resX_orig = img_czi.physical_pixel_sizes.X # in micrometers
+        self.img_resY_orig = img_czi.physical_pixel_sizes.Y
+        self.img_original = np.squeeze(img_czi.data)
+        print('Opened image: ', img_path, 'with shape: ', self.img_original.shape)
+        self.sigma = 3
+        self.low_threshold = 10
+        self.high_threshold = 25
+        self.background_intensity = 40
+        self.min_radius_um = 15 # min diameter defined by collaborators as d=30 micrometers. Min area A=pi*r^2
+        self.img = block_reduce(self.img_original, block_size=(self.downsampling_size, self.downsampling_size), func=np.mean)
+        self.update_resolutions()
+        self.img = apply_normalization(self.img)
+
+    def update_resolutions(self):
+        self.img_resX = self.downsampling_size * self.img_resX_orig
+        self.img_resY = self.downsampling_size * self.img_resY_orig
+
+    def update_donwnsampling(self, new_size):
+        self.downsampling_size = new_size
+        self.update_resolutions()
+
+    def update_sigma(self, new_sigma):
+        self.sigma = new_sigma
+
+    def update_low_threshold(self, new_low):
+        self.low_threshold = new_low
+
+    def update_high_threshold(self, new_high):
+        self.high_threshold = new_high
+
+    def min_pixel_area(self):
+        min_area = circle_area(self.min_radius_um)
+        min_area_pix = min_area / (self.img_resX * self.img_resY)
+        return round(min_area_pix)
+
+    def apply_morphologies(self):
+        # downsample 
+        self.img = block_reduce(self.img_original, block_size=(self.downsampling_size, self.downsampling_size), func=np.mean)   
+        self.update_resolutions()
+        # normalise
+        self.img = apply_normalization(self.img)
+        # get mask of well and background
+        mask = np.where(self.img<self.background_intensity,False,True)
+        # find edges in image
+        edges = canny(
+                    image=self.img,
+                    sigma=self.sigma,
+                    low_threshold=self.low_threshold,
+                    high_threshold=self.high_threshold,
+                    mask = mask)
+        # dilate edges
+        edges = ndi.binary_dilation(edges)
+        # fill holes
+        filled = ndi.binary_fill_holes(edges)
+        filled = erosion(filled)
+        filled = erosion(filled)
+        labels = label(filled)
+        region = regionprops(labels)
+        # remove objects larger than 30% of the image
+        for prop in region:
+            if prop.area > 0.3*self.img.shape[0]*self.img.shape[1]:
+                filled[prop.coords] = 0
+        # get min organoid size in pixels
+        min_size_pix = self.min_pixel_area()
+        filled = remove_small_objects(filled, min_size_pix)
+        segmentation = label(filled)
+        return segmentation
+
+    def find_median_cell_size(segmentation):
+        sizes=[]
+        for region in regionprops(segmentation):
+            shapes_areas = region.area
+            sizes.append(shapes_areas)
+        if len(sizes)!=0: avg_size = sum(sizes)/len(sizes)
+        else: avg_size = 0
+        return avg_size
+
+
+## confirmed with png image ##
+'''         
+img_jpg = Image.open('example_data/image001.png') #plt.imread('example_data/image001.png')
+#img_jpg  = Image.fromarray(img_jpg)
+img_jpg = img_jpg.convert('L')
+imgx = np.array(img_jpg)
+print('aaaa', img_jpg.size)
+#r, g, b = img_jpg[:,:,0], img_jpg[:,:,1], img_jpg[:,:,2]
+#img = img_jpg[:,:,3] #r*0.2126+g*0.7152+b*0.0722
+
+#print('AAAAAAA',[layer.name for layer in viewer.layers]) # ['Image', 'Segmentation', 'bboxes',]
+
+'''
--- viewer.py
+++ viewer.py
@@ -1,94 +1,22 @@
 import os
-import napari
 import argparse
-import numpy as np
-#import pandas as pd
-import matplotlib.pyplot as plt
-from scipy import ndimage as ndi
-from skimage.feature import canny
-from skimage.filters import gaussian
-from skimage.measure import regionprops,label
-from skimage.morphology import opening,remove_small_objects,closing,dilation,erosion
-from skimage.transform import resize
-import json
 import warnings
 warnings.filterwarnings("ignore")
+import json
+import napari
 from magicgui import magicgui
-from aicsimageio import AICSImage
-from skimage.measure import block_reduce
-from PIL import Image
-import math
 from typing import List
-
+import numpy as np
+from PIL import Image
+from OrgaCount import *
 
 def get_args():
     parser = argparse.ArgumentParser(description='Organoid counter')
     parser.add_argument('--image', default=False)
+    parser.add_argument('--save-screenshot', action='store_true')
     parser.add_argument('--auto-count', default=True)
     parser.add_argument('--output', default='annotations')
     return parser.parse_args()
-
-def find_median_cell_size(segmentation):
-    sizes=[]
-    for region in regionprops(segmentation):
-        shapes_areas = region.area
-        sizes.append(shapes_areas)
-    if len(sizes)!=0: avg_size = sum(sizes)/len(sizes)
-    else: avg_size = 0
-    return avg_size
-
-def setup_bboxes(res): #, median_cell_size):
-    bboxes = []
-    diameter1 = []
-    diameter2 = []
-    i=0
-    for region in regionprops(res):
-        minr, minc, maxr, maxc = region.bbox
-        bbox_rect = np.array([[minr, minc], [maxr, minc], [maxr, maxc], [minr, maxc]])
-        bboxes.append(bbox_rect)
-        diameter1.append(maxr-minr)
-        diameter2.append(maxc-minc)
-    bbox_properties = {'diameter1': diameter1, 'diameter2': diameter2}
-    text_parameters = {
-        'text': 'D1: {diameter1}\nD2: {diameter2}',
-        'anchor': 'upper_left',
-        'translation': [-5, 0],
-        'size': 8,
-        'color': 'green',
-    }
-    bboxes = np.array(bboxes)
-    return bbox_properties, text_parameters, bboxes
-
-def apply_morphologies(img, x_res, y_res, downsampling_size=1, sigma=3, low_threshold=10, high_threshold=25):
-    
-    img_min = np.min(img) # 31.3125 png 0
-    img_max = np.max(img) # 2899.25 png 178
-    img = (255 * (img - img_min) / (img_max - img_min)).astype(np.uint8)
-    x_res = x_res * downsampling_size
-    y_res = y_res * downsampling_size
-    min_size = math.pi * 15**2 # min diameter defined by collaborators as d=30 micrometers. Min area A=pi*r^2
-    min_size_pix = min_size / (x_res * y_res)
-
-    mask = np.where(img<40,False,True)
-    edges = canny(
-                image=img,
-                sigma=sigma,
-                low_threshold=low_threshold,
-                high_threshold=high_threshold,
-                mask = mask)
-    edges = ndi.binary_dilation(edges)
-    filled = ndi.binary_fill_holes(edges)
-    filled = erosion(filled)
-    filled = erosion(filled)
-    
-    region = regionprops(label(filled))
-    for prop in region:
-        if prop.area > 5000:
-            filled[prop.coords] = 0
-    
-    filled = remove_small_objects(filled, min_size_pix)
-    segmentation = label(filled)
-    return segmentation
 
 if __name__ == '__main__':
     args = get_args()
@@ -100,48 +28,50 @@ if __name__ == '__main__':
     else:
         input_dir = './'
         file_list = [args.image]
-    
-    @magicgui(
-            auto_call=True,         
-            sigma={"widget_type": "Slider", "max": 10},
-            low_threshold={"widget_type": "Slider", "max": 255},
-            high_threshold={"widget_type": "Slider", "max": 255})
-    def update_seg_res(sigma: int=3, low_threshold: int=10, high_threshold: int=25) -> List[napari.types.LayerDataTuple]:
-        segmentation = apply_morphologies(img, img_resX, img_resY, downsampling_size, sigma=sigma, low_threshold=low_threshold, high_threshold=high_threshold)
-        bbox_properties, text_parameters, bboxes = setup_bboxes(segmentation)
-        print('Number of organoids detected with automatic method: ', len(bboxes))
-        print('Sigma: ', sigma, ', Low threshold: ', low_threshold, ', High threshold: ', high_threshold)
-        if len(bboxes) > 0:
-            bbox_tuple = (bboxes, {'name': 'Organoids', 
-                                    'face_color': 'transparent',
-                                    'edge_color': 'magenta',
-                                    'properties': bbox_properties,
-                                    'text': text_parameters,
-                                    'shape_type': 'rectangle',
-                                    'edge_width': 3}, 'shapes')
-        else:
-            bbox_tuple = [([],{'name': 'Organoids', 'face_color': 'transparent', 'edge_color': 'magenta', 'edge_width': 3}, 'shapes')]
-        return [bbox_tuple]
 
     output_dir = '/Users/christina.bukas/Documents/AI_projects/datasets/organoid_counting/2022-01-17_HFibs_Organoids_EF_all_wells_trainingdata/results/CValgoRuolin'
     if not os.path.isdir(args.output):
         os.makedirs(args.output)
 
     for image_file in file_list:
-        img_czi = AICSImage(os.path.join(input_dir, image_file))
-        img_resX = img_czi.physical_pixel_sizes.X # in micrometers
-        img_resY = img_czi.physical_pixel_sizes.Y
-        img = np.squeeze(img_czi.data)
-        print('Opened image: ', image_file, 'with shape: ', img.shape)
-        img = block_reduce(img, block_size=(downsampling_size, downsampling_size), func=np.mean) # downsample       
+
+        @magicgui(
+        auto_call=True,         
+        sigma={"widget_type": "Slider", "min": 1, "max": 10},
+        downsampling={"widget_type": "Slider", "min": 1, "max": 10},
+        low_threshold={"widget_type": "Slider", "max": 255},
+        high_threshold={"widget_type": "Slider", "max": 255})
+    
+        def update_seg_res(sigma: int=3, downsampling: int=4, low_threshold: int=10, high_threshold: int=25) -> List[napari.types.LayerDataTuple]:
+            orga_count.update_sigma(sigma)
+            orga_count.update_donwnsampling(downsampling)
+            orga_count.update_low_threshold(low_threshold)
+            orga_count.update_high_threshold(high_threshold)
+            segmentation = orga_count.apply_morphologies()
+            bbox_properties, text_parameters, bboxes = setup_bboxes(segmentation)
+            print('Number of organoids detected with automatic method: ', len(bboxes))
+            print('Sigma: ', sigma, ', Low threshold: ', low_threshold, ', High threshold: ', high_threshold, 'Downsampling: ', downsampling)
+            img_tuple = (orga_count.img, {'name': 'Image'}, 'image')
+            if len(bboxes) > 0:
+                bbox_tuple = (bboxes, {'name': 'Organoids', 
+                                        'face_color': 'transparent',
+                                        'edge_color': 'magenta',
+                                        #'properties': bbox_properties,
+                                        #'text': text_parameters,
+                                        'shape_type': 'rectangle',
+                                        'edge_width': 3}, 'shapes')
+            else:
+                bbox_tuple = ([],{'name': 'Organoids', 'face_color': 'transparent', 'edge_color': 'magenta', 'edge_width': 3}, 'shapes')
+            return [img_tuple, bbox_tuple]
 
         raw_filename = image_file.split('.')[0]
         output_path = os.path.join(args.output, raw_filename+'.json')
-
+        orga_count = OrgaCount(input_dir, image_file, downsampling_size)
+        
         viewer = napari.Viewer()
-        img_layer = viewer.add_image(img, name='Image', colormap='gray')
+        img_layer = viewer.add_image(orga_count.img, name='Image', colormap='gray')
         if args.auto_count==True: 
-            segmentation = apply_morphologies(img, img_resX, img_resY, downsampling_size)
+            segmentation = orga_count.apply_morphologies()
             bbox_properties, text_parameters, bboxes = setup_bboxes(segmentation)#, median_cell_size)
             print('Number of organoids detected with automatic method: ', len(bboxes))
             #seg_layer = viewer.add_labels(segmentation, name='Segmentation')
@@ -149,23 +79,23 @@ if __name__ == '__main__':
                                             name='Organoids',
                                             face_color='transparent',  
                                             edge_color='magenta',
-                                            properties=bbox_properties,
-                                            text=text_parameters,
+                                            #properties=bbox_properties,
+                                            #text=text_parameters,
                                             shape_type='rectangle',
                                             edge_width=3) #30
-            screenshot=viewer.screenshot()
-            screenshot = Image.fromarray(screenshot)
-            screenshot.save(os.path.join(output_dir, raw_filename+'_screenshot.png'))
+            if args.save_screenshot:
+                screenshot=viewer.screenshot()
+                screenshot = Image.fromarray(screenshot)
+                screenshot.save(os.path.join(output_dir, raw_filename+'_screenshot.png'))
         else:
             shapes_layer = viewer.add_shapes(name='Organoids', face_color='transparent', edge_color='magenta', edge_width=3)
-            data = {}
         viewer.window.add_dock_widget(update_seg_res)
         # update the layer dropdown menu when the layer list changes
         viewer.layers.events.changed.connect(update_seg_res.reset_choices)
-        napari.run()
 
         @viewer.bind_key('s')
-        def store_centroids(viewer): 
+        def store_centroids(viewer):
+            data = {} 
             bboxes = viewer.layers['Organoids'].data # returns numpy array
             for i, bbox in enumerate(bboxes):
                 data.update({str(i): [list(bboxit) for bboxit in bbox]})
@@ -173,20 +103,6 @@ if __name__ == '__main__':
             with open(output_path, 'w') as outfile:
                 json.dump(data, outfile)        
             viewer.close()
+        
+        napari.run()
     
-
-
-
-## confirmed with png image ##
-'''         
-img_jpg = Image.open('example_data/image001.png') #plt.imread('example_data/image001.png')
-#img_jpg  = Image.fromarray(img_jpg)
-img_jpg = img_jpg.convert('L')
-imgx = np.array(img_jpg)
-print('aaaa', img_jpg.size)
-#r, g, b = img_jpg[:,:,0], img_jpg[:,:,1], img_jpg[:,:,2]
-#img = img_jpg[:,:,3] #r*0.2126+g*0.7152+b*0.0722
-
-#print('AAAAAAA',[layer.name for layer in viewer.layers]) # ['Image', 'Segmentation', 'bboxes',]
-
-'''
