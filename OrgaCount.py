fixed bug in diameter calculation, added cut-off as param and removed high low threshold, removed params from no automation

OrgaCount.py
@@ -57,7 +57,7 @@ def apply_normalization(img):
     return img_norm
 
 class OrgaCount():
-    def __init__(self, root_path, img_path, downsampling_size=4, sigma=3, low_threshold=10, high_threshold=25):
+    def __init__(self, root_path, img_path, downsampling_size=4, min_diameter_um=30, sigma=3, low_threshold=10, high_threshold=25):
         img_czi = AICSImage(os.path.join(root_path, img_path))
         self.img_resX_orig = img_czi.physical_pixel_sizes.X # in micrometers
         self.img_resY_orig = img_czi.physical_pixel_sizes.Y
@@ -68,7 +68,7 @@ class OrgaCount():
         self.low_threshold = low_threshold
         self.high_threshold = high_threshold
         self.background_intensity = 40
-        self.min_radius_um = 15 # min diameter defined by collaborators as d=30 micrometers. Min area A=pi*r^2
+        self.min_radius_um = min_diameter_um//2 #15 # min diameter defined by collaborators as d=30 micrometers. Min area A=pi*r^2
         self.img = block_reduce(self.img_original, block_size=(self.downsampling_size, self.downsampling_size), func=np.mean)
         self.update_resolutions()
         self.img = apply_normalization(self.img)
@@ -79,6 +79,9 @@ class OrgaCount():
     
     def get_current_downsampling(self):
         return self.downsampling_size
+
+    def update_min_organoid_size(self, min_diameter_size):
+        self.min_radius_um = min_diameter_size//2
 
     def update_donwnsampling(self, new_size):
         self.downsampling_size = new_size