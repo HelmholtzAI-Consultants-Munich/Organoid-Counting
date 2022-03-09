58518f86cdca6df5f21dcbde723d3727254deccb
Christina Bukas <ga27loj@mytum.de>
2022-03-09 11:40:45 +0100

    added documentation

--- OrgaCount.py
+++ OrgaCount.py
@@ -10,12 +10,30 @@ from skimage.morphology import remove_small_objects,erosion
 import cv2
 
 def add_text_to_img(img, organoid_number, downsampling=1):
+    '''
+    Adds the number of organoids detected as text to the image and returns it
+    Parameters
+    ----------
+    img: numpy array
+        The image on which text needs to be added
+    organoid_number: int
+        The number of organoids detected - to be added as text 
+    downsampling: int
+        the downsampling of the image will affect the text size
+    Returns
+    -------
+    img: numpy array
+        The image with text added to it
+    
+    '''
+    # define thickness and font size of the text depending on the downsampling rate
     thickness=2
     if downsampling==1: 
         fontSize = 10 #6
         thickness = 12 #4
     elif downsampling<4: fontSize = 3
     else: fontSize = 2
+    # add text to image
     img = cv2.putText(img, 
         'Organoids: '+str(organoid_number), 
         org=(round(img.shape[1]*0.05), round(img.shape[0]*0.1)),
@@ -26,16 +44,33 @@ def add_text_to_img(img, organoid_number, downsampling=1):
     return img
 
 def setup_bboxes(segmentation): 
+    '''
+    Given segmentation of organoids set up bounding boxes to visualise in napari
+    Parameters
+    ----------
+    segmentation: numpy array
+        The segmentation of organoids
+    Returns
+    -------
+    bbox_properties: dict
+        Holds the diameters of each detected object
+    text_parameters: dict
+        Holds parameters for visualisation of bounding boxes and parameters
+    bboxes: numpy array
+        An array where each entry is a bounding box in form [minr, minc], [maxr, minc], [maxr, maxc], [minr, maxc]
+    
+    '''
     bboxes = []
     diameter1 = []
     diameter2 = []
-    i=0
+    # for each detected object compute bounding box and min and max diameter
     for region in regionprops(segmentation):
         minr, minc, maxr, maxc = region.bbox
         bbox_rect = np.array([[minr, minc], [maxr, minc], [maxr, maxc], [minr, maxc]])
         bboxes.append(bbox_rect)
         diameter1.append(maxr-minr)
         diameter2.append(maxc-minc)
+    # set up properties of bounding box and text with parameters for napari visualisation
     bbox_properties = {'diameter1': diameter1, 'diameter2': diameter2}
     text_parameters = {
         'text': 'D1: {diameter1}\nD2: {diameter2}',
@@ -48,16 +83,81 @@ def setup_bboxes(segmentation):
     return bbox_properties, text_parameters, bboxes
 
 def circle_area(r):
+    '''Compute and return the area of a circle of radius r'''
     return math.pi * r**2 
 
 def apply_normalization(img):
+    '''Normalise and return img to range 0-255'''
     img_min = np.min(img) # 31.3125 png 0
     img_max = np.max(img) # 2899.25 png 178
     img_norm = (255 * (img - img_min) / (img_max - img_min)).astype(np.uint8)
     return img_norm
 
 class OrgaCount():
-    def __init__(self, root_path, img_path, downsampling_size=4, min_diameter_um=30, sigma=3, low_threshold=10, high_threshold=25):
+    '''
+    The OrgaCount class is used for segemnting lung organoids from brightfiled microscopy images.
+    This class includes  functions:
+    - '_update_resolutions' function, which updates the resolution in x and y given the downsampling size
+    - 'get_current_downsampling' function, which returns the current downsampling size
+    - 'update_min_organoid_size' function, which updates the minimun radius in um of organoids
+    - 'update_donwnsampling' function, which updates the downsampling size and resolution of image
+    - 'update_sigma' function, which updates the sigma used in the Canny Edge Detection algorithm
+    - 'update_low_threshold' function, which updates the low threshold used in the Canny Edge Detection algorithm
+    - 'update_high_threshold' function, which updates the high threshold used in the Canny Edge Detection algorithm
+    - '_min_pixel_area' function, which computes the minimum organoid area in pixels (approximate as circle)
+    - 'compute_real_values' function, which computes the diameters and area of the organoids in um (approx. ellipse)
+    - 'apply_morphologies' function, which detects the organoids in the image and returns the segmentation
+    Parameters
+    ----------
+        root_path : str
+            The directory in which image we wish to segment is stored
+        img_path : str
+            The name of the image we wish to segment. The image must be in czi format
+        downsampling_size : int, default=4
+            The downsamplign applied to the image as a pre-processign step
+        min_diameter_um : int, default=30
+            The minimum diameter of the organoids in um defined by the user (organoids are approximated as circles)
+        sigma : int, default=3
+            The sigma used in the Canny Edge Detection algorithm
+        low_threshold : int, default=10
+            The low hysteresis threshold used in the Canny Edge Detection algorithm
+        high_threshold : int, default=25  
+            The high hysteresis threshold used in the Canny Edge Detection algorithm 
+    Attributes
+    -------
+        img_resX_orig : float
+            The resultion in x axis of the original image read from disk
+        img_resY_orig : float
+            The resultion in y axis of the original image read from disk
+        img_original : np.array
+            The original image read from disk after removing dims of one
+        img_orig_norm: np.array
+            The original image normalised
+        downsampling_size : int
+            See parameters
+        sigma : int
+            See parameters
+        low_threshold : int
+            See parameters
+        high_threshold : int
+            See parameters
+        background_intensity : int
+            The intensity of the background of the image, around the plate
+        min_radius_um : int
+            The minimum radius of the organoids in um (organoids are approximated as circles)
+        img_resX: float
+            The resolution of the image in x axis after applying current downsampling
+        img_resY: float
+            The resolution of the image in y axis after applying current downsampling
+    '''
+    def __init__(self, 
+                root_path,
+                img_path, 
+                downsampling_size=4, 
+                min_diameter_um=30, 
+                sigma=3, 
+                low_threshold=10, 
+                high_threshold=25):
         img_czi = AICSImage(os.path.join(root_path, img_path))
         self.img_resX_orig = img_czi.physical_pixel_sizes.X # in micrometers
         self.img_resY_orig = img_czi.physical_pixel_sizes.Y
@@ -72,52 +172,86 @@ class OrgaCount():
         self.background_intensity = 40
         self.min_radius_um = min_diameter_um//2 #15 # min diameter defined by collaborators as d=30 micrometers. Min area A=pi*r^2
 
-    def update_resolutions(self):
+    def _update_resolutions(self):
+        ''' Update the resolution in x and y given the downsampling size'''
         self.img_resX = self.downsampling_size * self.img_resX_orig
         self.img_resY = self.downsampling_size * self.img_resY_orig
     
     def get_current_downsampling(self):
+        ''' Return the current downsampling size'''
         return self.downsampling_size
 
     def update_min_organoid_size(self, min_diameter_size):
+        ''' Update the minimun radius in um of organoids'''
         self.min_radius_um = min_diameter_size//2
 
     def update_donwnsampling(self, new_size):
+        ''' Update the downsampling size and resolution of image'''
         self.downsampling_size = new_size
-        self.update_resolutions()
+        self._update_resolutions()
 
     def update_sigma(self, new_sigma):
+        ''' Update the sigma used in the Canny Edge Detection algorithm'''
         self.sigma = new_sigma
 
     def update_low_threshold(self, new_low):
+        ''' Update the low threshold used in the Canny Edge Detection algorithm'''
         self.low_threshold = new_low
 
     def update_high_threshold(self, new_high):
+        ''' Update the high threshold used in the Canny Edge Detection algorithm'''
         self.high_threshold = new_high
 
-    def min_pixel_area(self):
+    def _min_pixel_area(self):
+        ''' Compute the minimum organoid area in pixels (approximate as circle) using the minimum radius of organoids 
+        defined by the user'''
         min_area = circle_area(self.min_radius_um)
         min_area_pix = min_area / (self.img_resX * self.img_resY)
         return round(min_area_pix)
     
-    def compute_real_values(self, d1, d2): # d1 and d2 are already in original resolution
+    def compute_real_values(self, d1, d2): 
+        '''
+        Given diameters of the organoid approximates as an ellipse compute the diameters and area of the organoids in um
+        Parameters
+        ----------
+        d1: int
+            The  first diameter of the organoid in pixels - organoids are approximated as ellipses
+        d2: int
+            The  second diameter of the organoid in pixels - organoids are approximated as ellipses
+        Returns
+        -------
+        d1: float
+            The  first diameter of the organoid in um
+        d2: float
+            The  second diameter of the organoid in um
+        area: float
+            The area of the organoid in um - approximated as an ellipse
+        '''
+        # d1 and d2 are already in original resolution
         d1 = d1 * self.img_resX_orig
         d2 = d2 * self.img_resY_orig
         area = math.pi * d1 * d2
         return d1, d2, area
     
     def apply_morphologies(self):
+        '''
+        Detect the organoids in the image and return the segmentation
+        Returns
+        -------
+        segmentation: numpy array
+            The resulting segmentation of the image
+        '''
         # downsample 
-        self.img = block_reduce(self.img_original, block_size=(self.downsampling_size, self.downsampling_size), func=np.mean)   
-        print('Image resampled to size: ', self.img.shape)
-        self.update_resolutions()
+        img = block_reduce(self.img_original, block_size=(self.downsampling_size, self.downsampling_size), func=np.mean)   
+        print('Image resampled to size: ', img.shape)
+        self._update_resolutions()
         # normalise
-        self.img = apply_normalization(self.img)
+        img = apply_normalization(img)
         # get mask of well and background
-        mask = np.where(self.img<self.background_intensity,False,True)
+        mask = np.where(img<self.background_intensity,False,True)
         # find edges in image
         edges = canny(
-                    image=self.img,
+                    image=img,
                     sigma=self.sigma,
                     low_threshold=self.low_threshold,
                     high_threshold=self.high_threshold,
@@ -132,34 +266,10 @@ class OrgaCount():
         region = regionprops(labels)
         # remove objects larger than 30% of the image
         for prop in region:
-            if prop.area > 0.3*self.img.shape[0]*self.img.shape[1]:
+            if prop.area > 0.3*img.shape[0]*img.shape[1]:
                 filled[prop.coords] = 0
         # get min organoid size in pixels
-        min_size_pix = self.min_pixel_area()
+        min_size_pix = self._min_pixel_area()
         filled = remove_small_objects(filled, min_size_pix)
         segmentation = label(filled)
         return segmentation
-
-'''
-def find_median_cell_size(segmentation):
-    sizes=[]
-    for region in regionprops(segmentation):
-        shapes_areas = region.area
-        sizes.append(shapes_areas)
-    if len(sizes)!=0: avg_size = sum(sizes)/len(sizes)
-    else: avg_size = 0
-    return avg_size
-
-## confirmed with png image ##
-         
-img_jpg = Image.open('example_data/image001.png') #plt.imread('example_data/image001.png')
-#img_jpg  = Image.fromarray(img_jpg)
-img_jpg = img_jpg.convert('L')
-imgx = np.array(img_jpg)
-print('aaaa', img_jpg.size)
-#r, g, b = img_jpg[:,:,0], img_jpg[:,:,1], img_jpg[:,:,2]
-#img = img_jpg[:,:,3] #r*0.2126+g*0.7152+b*0.0722
-
-#print('AAAAAAA',[layer.name for layer in viewer.layers]) # ['Image', 'Segmentation', 'bboxes',]
-
-'''
--- viewer.py
+++ viewer.py
@@ -27,6 +27,7 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
+    # check if a single image or a directory of images has been given
     if os.path.isdir(args.image):
         input_dir = args.image
         file_list = os.listdir(args.image)
@@ -34,12 +35,13 @@ if __name__ == '__main__':
     else:
         input_dir = './'
         file_list = [args.image]
-
+    # create output directory if not existing
     if not os.path.isdir(args.output):
         os.makedirs(args.output)
-
+    
+    # for image provided
     for image_file in file_list:
-        
+        # set up execute button in napari viewer, which when clicked will update the number of organoids displayed in text
         @magicgui(call_button="execute")
         def update_display_text(bboxes: ShapesData) -> List[napari.types.LayerDataTuple]:
             img = orga_count.img_orig_norm.copy()
@@ -51,55 +53,65 @@ if __name__ == '__main__':
                         'shape_type': 'rectangle',
                         'edge_width': 12}, 'shapes')      
             return [img_tuple, bbox_tuple]
-
+        # set up parameter sliders in napari viewer, so user can change algorithm parameters and choose best results
         @magicgui(
         auto_call=True,         
         downsampling={"widget_type": "Slider", "min": 1, "max": 10},
         min_diameter={"widget_type": "Slider", "min": 10, "max": 100},
         sigma={"widget_type": "Slider", "min": 1, "max": 10})
+        # low and high threshold removed for now as not helpful for user
         #low_threshold={"widget_type": "Slider", "max": 100},
         #high_threshold={"widget_type": "Slider", "max": 100})
         def update_seg_res(downsampling: int=4, min_diameter: int=30, sigma: int=3) -> List[napari.types.LayerDataTuple]: #, low_threshold: int=10, high_threshold: int=25
+            # update all parameters in OrgaCount instance
             orga_count.update_donwnsampling(downsampling)
             orga_count.update_min_organoid_size(min_diameter)
             orga_count.update_sigma(sigma)
             #orga_count.update_low_threshold(low_threshold)
             #orga_count.update_high_threshold(high_threshold)
+            
+            # get segmentation
             segmentation = orga_count.apply_morphologies()
             _, _, bboxes = setup_bboxes(segmentation)
-            bboxes *= orga_count.get_current_downsampling() # reset to original resolution
+            # reset box coordinates to original resolution
+            bboxes *= orga_count.get_current_downsampling() 
             print('Number of organoids detected with automatic method: ', len(bboxes))
             print('Downsampling: ', downsampling, 'Minimum organoid diameter: ', min_diameter, ', Sigma: ', sigma) #', Low threshold: ', low_threshold, ', High threshold: ', high_threshold,
+            # display image and bounding boxes in naapri viewer
             img = orga_count.img_orig_norm.copy()
             img = add_text_to_img(img, len(bboxes)) #, downsampling)
             img_tuple = (img, {'name': 'Image'}, 'image')
             if len(bboxes) > 0:
-                #if downsampling<3: edge_width=5
-                #else: edge_width=3
-                edge_width=12
                 bbox_tuple = (bboxes, {'name': 'Organoids', 
                                         'face_color': 'transparent',
                                         'edge_color': 'magenta',
                                         'shape_type': 'rectangle',
-                                        'edge_width': edge_width}, 'shapes')
+                                        'edge_width': 12}, 'shapes')
             else:
                 bbox_tuple = ([],{'name': 'Organoids', 'face_color': 'transparent', 'edge_color': 'magenta', 'edge_width': 12}, 'shapes')
             return [img_tuple, bbox_tuple]
- 
+        
+        # create filenames for two output files
         raw_filename = os.path.split(image_file)[-1]
         raw_filename = raw_filename.split('.')[0]
         output_path_json = os.path.join(args.output, raw_filename+'.json')
         output_path_csv = os.path.join(args.output, raw_filename+'.csv')
+
+        # initialise OrgaCount instance with current image
         orga_count = OrgaCount(input_dir, image_file, args.downsample, args.min_diameter, args.sigma, args.low_threshold, args.high_threshold)
         
+        # initialise napari viewer
         viewer = napari.Viewer()
         img = orga_count.img_orig_norm.copy()
+        # if auto_count is true get image segmentation, else open napari viewer so user can annotate
         if args.auto_count==True: 
+            # get image segmentation and get bounding boxes
             segmentation = orga_count.apply_morphologies()
-            _, _, bboxes = setup_bboxes(segmentation)#, median_cell_size)
+            _, _, bboxes = setup_bboxes(segmentation)
             bboxes *= orga_count.get_current_downsampling()
             print('Number of organoids detected with automatic method: ', len(bboxes))
-            img = add_text_to_img(img, len(bboxes)) #, orga_count.downsampling_size)
+            img = add_text_to_img(img, len(bboxes))
+            # add image and bounding boxes layer to napari viewer
             img_layer = viewer.add_image(img, name='Image', colormap='gray')
             shapes_layer = viewer.add_shapes(bboxes, 
                                             name='Organoids',
@@ -107,6 +119,7 @@ if __name__ == '__main__':
                                             edge_color='magenta',
                                             shape_type='rectangle',
                                             edge_width=12)
+            # add parameters window to napari viewer
             viewer.window.add_dock_widget(update_seg_res)
             # update the layer dropdown menu when the layer list changes
             viewer.layers.events.changed.connect(update_seg_res.reset_choices)
@@ -116,26 +129,28 @@ if __name__ == '__main__':
         # update the text displaying the number of organoids
         viewer.window.add_dock_widget(update_display_text)
         viewer.layers.events.changed.connect(update_display_text.reset_choices)
-
+        
+        # once user hit the 's' key on the keyboard save results and close viewer
         @viewer.bind_key('s')
         def store_centroids(viewer):
+            # take a screenshot of current visualisation - overlay image and bounding boxes
             if args.save_screenshot:
                 screenshot=viewer.screenshot()
                 imsave(os.path.join(args.output, raw_filename+'_screenshot.png'), screenshot)
             data_json = {} 
             data_csv = []
             bboxes = viewer.layers['Organoids'].data # returns numpy array
+            # save diameters and area of organoids (approximated as ellipses)
             for i, bbox in enumerate(bboxes):
                 d1 = abs(bbox[0][0] - bbox[2][0])
                 d2 = abs(bbox[0][1] - bbox[2][1])
                 d1, d2, area = orga_count.compute_real_values(d1,d2)
                 data_csv.append([i, round(d1,3), round(d2,3), round(area,3)])
-                #bbox *= orga_count.get_current_downsampling()
                 data_json.update({str(i): [list(bboxit) for bboxit in bbox]})
-            #write to csv
+            #write diameters and area to csv
             df = pd.DataFrame(data_csv, columns =['OrganoidID', 'D1[um]','D2[um]', 'Area [um^2]']) 
             df.to_csv(output_path_csv, index=False)
-            #write to json
+            #write bbox coordinates to json
             with open(output_path_json, 'w') as outfile:
                 json.dump(data_json, outfile)        
             viewer.close()
